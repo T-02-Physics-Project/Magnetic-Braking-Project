f4cba299c7eb85db1e2df6f2c4b45e4bd30fe523
Ben Andrews <benafandrews@gmail.com>
2021-02-15 14:39:30 +0000

    Small fix and changed graphs
    Filter was missing 3rd set of coil current readings, added graphing to 3 different subplots for the three different sets of readings. Need to work out how to make first set of readings less crowded.

--- analysis/curvefitting/example.py
+++ analysis/curvefitting/example.py
@@ -22,7 +22,7 @@ column_headers = list(cleaned_data.columns)                     # Get all column
 # To understand why I chose this filter, check the improved_table.xlsx file I uploaded. I've grouped all the data within and given the columns unique names.
 # A, B, or C simply refers to which set of current readings it relates to.
 # Look up regular expression or regex to learn more.
-filter = re.compile(r'[R][A-B]\d+')
+filter = re.compile(r'[R][A-C]\d+')
 
 # Add all headers to list if they match the filter parameters defined above.
 # We don't need time data as we just create a list of numbers with matching length to the angular velocity data
@@ -42,16 +42,44 @@ def getArrToFirstZero(arr):
 # A list of time values with matching length to the data is created starting from 0 seconds.
 # Curve is fitted, very simplistic plot.
 # Until we know how we're going to do our error analysis it seems excessive to put in error bars that won't exist as well.
+fig = plt.figure(figsize=(30, 10))
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+n = 1
 for measurement in angular_velocity_headers:
     angular_velocities = getArrToFirstZero(cleaned_data[measurement].values)
     times = [value for value in range(len(angular_velocities))]
 
     popt, pcov = curve_fit(line, times, angular_velocities, maxfev=100000)
 
-    times2 = np.linspace(0, max(times)+1, 1000)
+    times2 = np.linspace(0, max(times), 1000)
     curve_fit_vals = line(times2, popt[0], popt[1], popt[2])
 
-    plt.plot(times2, curve_fit_vals, "r--")
-    plt.plot(times, angular_velocities, "kx")
-    plt.show()
+    if measurement[1] == "A":
+        ax1.plot(times2, curve_fit_vals, "--")
+        ax1.plot(times,
+                    angular_velocities,
+                    "x",
+                    markersize=7
+                    )
+    elif measurement[1] == "B":
+        
+        ax2.plot(times2, curve_fit_vals, "--")
+        ax2.plot(times,
+                    angular_velocities,
+                    "x",
+                    markersize=7
+                    )
+    elif measurement[1] == "C":
+        ax3.plot(times2, curve_fit_vals, "--")
+        ax3.plot(times,
+                    angular_velocities,
+                    "x",
+                    markersize=7
+                    )
+
+    n += 1
+plt.show()
 
